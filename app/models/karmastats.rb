4dbe06aa6a6469db908d12aa4a8846fb82273a59
Timothy S. Williams <tiwillia@redhat.com>
2013-08-18 02:19:06 -0400

    Clean up

--- app/models/karmastats.rb
+++ app/models/karmastats.rb
@@ -1,19 +1,5 @@
 #!/usr/bin/env ruby
 
 class KarmaStats < ActiveRecord::Base
-
-self.table_name = "karmastats"
-
-# This following method is unused, but is a remnant of another way
-# to do this. Or something. idk. Brain = fried.
-=begin
-def get(user_id)
-  total = 0
-  Karma.find_each(recipient_id: user_id) do |k|
-    total = (total + k.amount)
-  end
-  total 
-end
-=end
-
+  self.table_name = "karmastats"
 end
