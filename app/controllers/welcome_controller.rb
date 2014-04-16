b66baf043cd971e836c863e033576ea9c260f123
Timothy Williams <tiwillia@timdev.usersys.redhat.com>
2014-04-15 21:12:26 -0400

    Added diagnostic to assist in database migration

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
+    User.all.each do |user|
+  if user.user.nil? || user.user.downcase == user.user
+    next
+  end
+  if User.where(:user => user.user.downcase)
+    if KarmaStats.where(:user_id => user.id)
+      Rails.logger.info "#{user.user} exists and conflicts"
+    end
+  end
+end
+
   end
 end
